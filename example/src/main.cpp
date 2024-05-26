3b27300c2e912f048bae7d7232ad8d10ac4e07cf
Alfred Bratterud <alfred.bratterud@gmail.com>
2024-05-26 08:37:27 +0200

    Test with main instead of Service::start

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,7 +1,18 @@
 #include <os>
 #include <service>
 
-void Service::start(const std::string& args){
-  printf("Args = %s\n", args.c_str());
+class Universal {
+public:
+  Universal(){
+    printf("\"There is no universal class.\"\n\t-Bjarne Stroustrup 😎\n");
+  }
+} nonsense;
+
+/*void Service::start(const std::string& args){
+  printf("Args = '%s'\n", args.c_str());
   printf("Try giving the service less memory, eg. 5MB in vm.json\n");
+  }*/
+
+int main(){
+  printf("We can also go mainstream\n");
 }
